f91016b92be2f4c0f134c550d6f7e4a2fd6162c1
xfy196 <xfy196@outlook.com>
2021-06-12 17:53:22 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import resolve from 'rollup-plugin-node-resolve';
 import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    resolve()
   ],
   base: "/",
   resolve: {
